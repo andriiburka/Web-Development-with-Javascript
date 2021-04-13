0b06d23e4bbc8f28721d98c8f5c5fbc63d5ea6a9
Andrii Burka <andriiburka@gmail.com>
2021-04-14 01:29:13 +0300

    ...

--- upload.sh
+++ upload.sh
@@ -1,9 +1,6 @@
-# shellcheck disable=SC2164
 cd
 cd WebstormProjects/education-javascript
 git add .
-# shellcheck disable=SC2162
-read msg
-git commit -m "$msg"D
+git commit -m "..."
 git push -u origin master
 cd
